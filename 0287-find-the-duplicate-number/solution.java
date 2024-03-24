8d1a0cc487763cf51c7939b96c16b3d52ee2dc49
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-03-24 17:19:26 +0000

    Sync LeetCode submission Runtime - 2 ms (95.18%), Memory - 59.7 MB (6.40%)

---
+++ 0287-find-the-duplicate-number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int[] freq = new int[nums.length+1];
+
+        for(int i : nums){
+            freq[i]++;
+        }
+        for(int i = 0; i < freq.length; i++){
+            
+            if(freq[i] >=2){
+                return i;
+            }
+            
+        }
+        return 0;
+    }
+}
