caa45e5de108d1e716f697b29efb5d64a8860a19
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-04-14 09:32:55 +0000

    Sync LeetCode submission Runtime - 7 ms (60.44%), Memory - 43.2 MB (5.36%)

---
+++ 0257-binary-tree-paths/solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> result = new ArrayList<>();
+        dfs(root, "", result);
+        return result;
+    }
+
+    private void dfs(TreeNode node, String path, List<String> result) {
+        if (node == null) return;
+        
+        path += node.val;
+
+        
+        if (node.left == null && node.right == null) {
+            result.add(path);
+        } else {
+            path += "->";
+            dfs(node.left, path, result);
+            dfs(node.right, path, result);
+        }
+    }
+}
