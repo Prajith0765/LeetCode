494bcbcb197caffd763e964c9ab8062437a36cc2
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-04-18 16:14:10 +0000

    Sync LeetCode submission Runtime - 5 ms (51.42%), Memory - 46 MB (14.17%)

---
+++ 0463-island-perimeter/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int ans=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    if(i==0){
+                        ans++;
+                    }
+                    if(j==0){
+                        ans++;
+                    }
+                    if(j>0 && grid[i][j-1]==0){
+                        ans++;
+                    }
+                    if(j<grid[0].length-1 && grid[i][j+1]==0){
+                        ans++;
+                    }
+                    if(i>0 && grid[i-1][j]==0){
+                        ans++;
+                    }
+                    if(i<grid.length-1 && grid[i+1][j]==0){
+                        ans++;
+                    }
+                    if(i==grid.length-1){
+                        ans++;
+                    }
+                    if(j==grid[0].length-1){
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
