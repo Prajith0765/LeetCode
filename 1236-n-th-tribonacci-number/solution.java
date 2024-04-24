21948cbd7564913b48b0c53380eb6cee02fe914b
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2024-04-24 17:48:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.1 MB (56.37%)

---
+++ 1236-n-th-tribonacci-number/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int tribonacci(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, Map<Integer, Integer> memo) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        
+        int result;
+        if (n == 0) {
+            result = 0;
+        } else if (n == 1 || n == 2) {
+            result = 1;
+        } else {
+            result = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        }
+        
+        memo.put(n, result);
+        return result;
+    }
+}
