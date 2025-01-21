da072d8821970addd3e33538e6ef27c2e3012016
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-01-21 11:58:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.7 MB (84.26%)

--- 0258-add-digits/solution.java
+++ 0258-add-digits/solution.java
@@ -1,10 +1,10 @@
 class Solution {
     public int addDigits(int num) {
-        if(num < 10) return num;
+        if(num < 10)  return num;
         int ans = 0;
         while(num != 0){
-            ans = ans + (num % 10);
-            num /= 10;
+            ans += (num % 10);
+            num = num / 10;
         }
         return addDigits(ans);
     }
