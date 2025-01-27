1da8e2ae284b7f3c61f16c8c8262f54f06329589
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-01-27 09:26:40 +0000

    Sync LeetCode submission Runtime - 5 ms (52.63%), Memory - 43.3 MB (68.88%)

---
+++ 2800-minimum-string-length-after-removing-substrings/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> st = new Stack();
+
+        for(char ch : s.toCharArray()){
+            if(!st.empty() && ((st.peek() == 'A' && ch == 'B') || (st.peek() == 'C' && ch == 'D'))){
+                st.pop();
+            } else{
+                st.push(ch);
+            }
+        }
+        return st.size();
+    }
+}
