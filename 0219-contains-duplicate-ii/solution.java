9b537b7c781bb142a292e7c5df64e18fe4c659da
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-01-30 16:16:45 +0000

    Sync LeetCode submission Runtime - 1174 ms (8.27%), Memory - 58.6 MB (9.11%)

---
+++ 0219-contains-duplicate-ii/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for(int i=0;i< nums.length-1; i++){
+            for(int j=i+1;j<nums.length && j<=i+k;j++){
+                if(nums[i]==nums[j]) return true;
+            }
+        }
+        return false;
+    }
+}
