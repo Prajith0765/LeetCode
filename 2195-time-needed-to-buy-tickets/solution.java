24579280298084a6b8328b903d6cbdbac1ae7459
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-02-05 15:59:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.5 MB (57.33%)

--- 2195-time-needed-to-buy-tickets/solution.java
+++ 2195-time-needed-to-buy-tickets/solution.java
@@ -1,25 +1,18 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-       Queue<Integer> queue = new LinkedList<>();
+      
+        if (k < 0 || k >= tickets.length) {
+            return 0;
+        }
 
-       for(int i = 0 ; i < tickets.length; i++){
-        queue.add(i);
-       }
-       int time = 0;
-       while(!queue.isEmpty()){
-        time++;
-        int person = queue.poll();
-        if(tickets[person] >= 1){
-            tickets[person]--;
+        int ans = 0;
+        for (int i = 0; i < tickets.length; ++i) {
+            // Case 1: Person k, or each of those in front, will contribute min(tickets[k], tickets[i]) seconds.
+            // Case 2: Each behind person k will contribute min(tickets[k] - 1, tickets[i]) seconds.
+            // The reason is that we don't care about the extra time for people who want more tickets than person k.
+            ans += Math.min(tickets[k] - (i > k ? 1 : 0), tickets[i]);
         }
-        if(person == k && tickets[person] == 0){
-            break;
-        }
-        if(person != k && tickets[person] == 0){
-            continue;
-        }
-        queue.add(person);
-       }
-       return time;
+
+        return ans;
     }
 }
