f8294eb1829e1f982e91c5e64a287b5f4e9ad4d4
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-02-20 14:18:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.4 MB (86.88%)

---
+++ 2107-find-unique-binary-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        StringBuilder result = new StringBuilder();
+        for(int i = 0; i < nums.length; i++){
+            result.append(nums[i].charAt(i) == '0' ? '1' : '0');
+        }
+        return result.toString();
+    }
+}
