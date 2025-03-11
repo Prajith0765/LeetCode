80d85c5ae4d9351e9e19d899b8f764cfd6dd674f
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-03-11 16:47:14 +0000

    Sync LeetCode submission Runtime - 54 ms (87.23%), Memory - 46.5 MB (43.48%)

---
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/solution.java
@@ -0,0 +1,51 @@
+class Solution {
+    public long countOfSubstrings(String word, int k) {
+        int[][] frequencies = new int[2][128];
+        frequencies[0]['a'] = 1;
+        frequencies[0]['e'] = 1;
+        frequencies[0]['i'] = 1;
+        frequencies[0]['o'] = 1;
+        frequencies[0]['u'] = 1;
+
+        long response = 0;
+
+        int currentK = 0;
+        int vowels = 0;
+        int extraLeft = 0;
+        for (int right = 0, left = 0; right < word.length(); right++) {
+            char rightChar = word.charAt(right);
+
+            if (frequencies[0][rightChar] == 1 && ++frequencies[1][rightChar] == 1) {
+                vowels++;
+            } else if (frequencies[0][rightChar] == 0) {
+                currentK++;
+            }
+
+            while (currentK > k) {
+                char leftChar = word.charAt(left);
+
+                if (frequencies[0][leftChar] == 1 && --frequencies[1][leftChar] == 0) {
+                    vowels--;
+                } else if (frequencies[0][leftChar] == 0) {
+                    currentK--;
+                }
+                left++;
+                extraLeft = 0;
+            }
+
+            //try to shrink
+            while (vowels == 5 && currentK == k && left < right && frequencies[0][word.charAt(left)] == 1 && frequencies[1][word.charAt(left)] > 1) {
+                extraLeft++;
+                frequencies[1][word.charAt(left)]--;
+                left++;
+            }
+
+            if (currentK == k && vowels == 5) {
+                response++;
+                response += extraLeft;
+            }
+        }
+
+        return response;
+    }
+}
