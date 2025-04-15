365924861cfb01666af10cb0641dcbb205dea412
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-04-15 12:17:27 +0000

    Sync LeetCode submission Runtime - 6 ms (11.83%), Memory - 45.4 MB (57.05%)

--- 0268-missing-number/solution.java
+++ 0268-missing-number/solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
-        int  i = 0;
-        while(i < nums.length){
-            if(nums[i] != i){
-                return i;
-            }
-            i++;
+        int xor1 = 0;
+        int xor2 = 0;
+        int n = nums.length;
+        for(int i = 0; i < n ; i++){
+            xor2 ^= nums[i];
+            xor1 ^= i;
         }
-        return nums.length;
+        xor1 ^= n;
+        return (xor1 ^ xor2);
     }
 }
