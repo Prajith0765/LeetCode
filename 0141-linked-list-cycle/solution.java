547c7c950c559441aca682fbe13f1ff329534156
P.Prajith <129357819+Prajith0765@users.noreply.github.com>
2025-08-22 14:26:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.3 MB (89.18%)

---
+++ 0141-linked-list-cycle/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        //Floyd’s Cycle-Finding Algorithm // fast slow approach // 2 pointers
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
